dbad7a27e55e77cc033cdc1c98b1202cb46a20e5
Jessica Thais da Silva Martins <jessicat.silvap@gmail.com>
2025-05-04 10:22:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function carConteudo(){
+    document.getElementById("conteudo").innerHTML = "<p>Se o seu código não funcionar, não se preocupe, é só mais um bug para a história... ou um erro de digitação bem grande. Mas lembre-se, a cada bug corrigido, você está um passo mais próximo de ser o próximo Bill Gates ou pelo menos ter a mesma camisa.</p>"
+}
